81deb7873296b609c6fc8f34a587da7c7ae83e0d
Saud Zaman <saudzaman160@gmail.com>
2021-06-05 00:56:01 +0100

    first-commit

---
+++ app/src/main/java/com/android/whatsapp/myclasses/Friend.java
@@ -0,0 +1,8 @@
+package com.android.whatsapp.myclasses;
+
+
+
+public class Friend extends User{
+    public String id;
+    public String idRoom;
+}
---
+++ app/src/main/java/com/android/whatsapp/myclasses/Status.java
@@ -0,0 +1,13 @@
+package com.android.whatsapp.myclasses;
+
+
+
+public class Status{
+    public boolean isOnline;
+    public long timestamp;
+
+    public Status(){
+        isOnline = false;
+        timestamp = 0;
+    }
+}
